e9c1744d4ffa5ccf53e1bffd952a22bfff24953f
José Alfredo Arroyo Gómez <arroyo.alfredo.1pm@gmail.com>
2025-08-24 03:23:49 -0600

    curso angular 2025 seccion 12: 153. Date Pipe

---
+++ udemy/fernando-herrera/angular-desde-cero/05-pipes-app/src/app/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,24 @@
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
+
+@Component({
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
+  templateUrl: './basic-page.component.html',
+})
+export default class BasicPageComponent {
+  nameLower = signal('freddy');
+  nameUpper = signal('FREDDY');
+  fullName = signal('AlFrEDo ARRoYO');
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+    }, 1000);
+
+    onCleanup(() => {
+      clearInterval(interval);
+    })
+  });
+}
---
+++ udemy/fernando-herrera/angular-desde-cero/05-pipes-app/src/app/pages/numbers-page/numbers-page.component.ts
@@ -0,0 +1,11 @@
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
+
+@Component({
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
+  templateUrl: './numbers-page.component.html',
+})
+export default class NumbersPageComponent {
+  totalSells = signal(2_433_423.5367);
+  percent = signal(0.4856);
+}
